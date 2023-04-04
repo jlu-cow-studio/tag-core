6891a73ce10821c3200c6ab9f12d02e1c035980d
waynelee-lwc <1418551940@qq.com>
2023-04-04 16:56:15 +0800

    update handlers

--- handler/UpdateItemTagsHandler.go
+++ handler/UpdateItemTagsHandler.go
@@ -3,9 +3,27 @@ package handler
 import (
 	"context"
 
+	"github.com/jlu-cow-studio/common/dal/rpc/base"
 	"github.com/jlu-cow-studio/common/dal/rpc/tag_core"
+	"github.com/jlu-cow-studio/tag-core/biz"
 )
 
 func (h *Handler) UpdateItemTags(ctx context.Context, req *tag_core.UpdateItemTagsRequest) (res *tag_core.UpdateItemTagsResponse, err error) {
+	res = &tag_core.UpdateItemTagsResponse{
+		Base: &base.BaseRes{
+			Message: "",
+			Code:    "498",
+		},
+	}
+
+	if err = biz.UpdateItemTags(req.ItemId, req.TagList); err != nil {
+		res.Base.Message = err.Error()
+		res.Base.Code = "400"
+		return res, nil
+	}
+
+	res.Base.Code = "200"
+	res.Base.Message = ""
+
 	return
 }
--- handler/UpdateUserTagsHandler.go
+++ handler/UpdateUserTagsHandler.go
@@ -3,10 +3,27 @@ package handler
 import (
 	"context"
 
+	"github.com/jlu-cow-studio/common/dal/rpc/base"
 	"github.com/jlu-cow-studio/common/dal/rpc/tag_core"
+	"github.com/jlu-cow-studio/tag-core/biz"
 )
 
 func (h *Handler) UpdateUserTags(ctx context.Context, req *tag_core.UpdateUserTagsRequest) (res *tag_core.UpdateUserTagsResponse, err error) {
+	res = &tag_core.UpdateUserTagsResponse{
+		Base: &base.BaseRes{
+			Message: "",
+			Code:    "498",
+		},
+	}
+
+	if err = biz.UpdateUserTags(req.UserId, req.TagList); err != nil {
+		res.Base.Message = err.Error()
+		res.Base.Code = "400"
+		return res, nil
+	}
+
+	res.Base.Code = "200"
+	res.Base.Message = ""
 
 	return
 }
